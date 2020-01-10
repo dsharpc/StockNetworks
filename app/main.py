4356cf887a0d2dac9afbe0c0f838c8788a746703
Daniel Sharp <daniel@applied-data.science>
2020-01-10 10:34:55 +0000

    Scrapped unnecessary functions and cleaned up code

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
-from vantage_api import download_stock_list, get_symbols, fetch_price, symbol_match_status, manual_symbols
+from vantage_api import fetch_price, RateLimitExceeded
+from lse_scraper import get_symbols
 from utils import get_pg_engine, read_table
 import argparse
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process Alpha Vantage API stocks')
-    parser.add_argument('-d','--download', action='store_true', help='Download list of companies to database')
-    parser.add_argument('-gs','--get_symbols', type=int, default=0, help='Match the stocks\' symbols and how many to match')
+    parser.add_argument('-gs','--get_symbols', action='store_true', help='Scrape the London Stock Exchange website for stock names and symbols')
     parser.add_argument('-f','--fetch_price', action='store_true', help='Fetch price of existing stocks')
-    parser.add_argument('-m','--match_status', action='store_true', help='Status of unmatched companies, how many are there?')
-    parser.add_argument('-u','--update_symbols', action='store_true', help='Manually update symbols')
 
     args = parser.parse_args()
 
-    if args.download:
-        download_stock_list()
-
-    if args.get_symbols > 0:
-        get_symbols(args.get_symbols)
+    if args.get_symbols:
+        get_symbols()
 
     if args.fetch_price:
         stocks = read_table('symbols')
@@ -28,9 +23,6 @@ if __name__ == "__main__":
             except AssertionError as e:
                 print(e)
                 pass
-
-    if args.match_status:
-        symbol_match_status()
-
-    if args.update_symbols:
-        manual_symbols()
+            except RateLimitExceeded as e:
+                print(e)
+                break
--- app/vantage_api.py
+++ app/vantage_api.py
@@ -21,98 +21,11 @@ logging.basicConfig(level=logging.INFO,
 logger=logging.getLogger() 
 
 VANTAGE_API_KEY = os.environ['VANTAGE_API_KEY']
-STOCK_LIST = os.environ['STOCK_LIST']
 
-
-def download_stock_list():
-    """
-    This function will download the list of stocks from the s3 storage and writes it to the companies table in the database
-    """
-    logging.info("Downloading stock list")
-    logging.info("Reading full list from cloud")
-    df = pd.read_csv(STOCK_LIST)
-
-    engine = get_pg_engine()
-
-    # Write new stocks
-    logging.info("Writing new stocks to db")
-    df = clean_columns(df)
-    df['companyname'] = df['companyname'].str.replace('\'','')
-    df.to_sql('companies', engine, if_exists='replace')
-    logging.info("Stock list inserted in database")
-
-def get_symbols(num = 0):
-    """
-    (FUNCTION DEPRECATED - NOW DONE BY LSE SCRAPER SCRIPT)
-    This function will match the top n stocks ordered by their market cap, where these n stocks don't yet exist in the database.
-    Its only argument is the number of stocks to match. This is default -1, which means all the stocks.
-    It will also check if these stocks already exist inside the database to avoid making unnecesary API calls.
-    """    
-    logging.info("Starting symbol matching")
-    # Make sure that the number of stocks being obtained is correct
-    assert num >= 0, 'Cannot match a negative number of stocks'
-    assert isinstance(num, int), 'Number of stocks has to be an integer'
-
-    if num > 500:
-        logging.warning("This execution might fail due to API limiting as Alpha Vantage API allows a maximum of 500 calls per day")        
-
-
-    # Create table to store the symbols in if it doesn't exist
-    engine = get_pg_engine()
-    engine.execute("CREATE TABLE IF NOT EXISTS symbols (companyname text, symbol text)")
-    
-    df = read_table('companies')
-    df_exist = read_table('symbols')
-
-    # Of the total list, filter those that are already in the database
-    df = df[~df['companyname'].isin(df_exist['companyname'])]
-
-    df = df.sort_values(by='companymarketcapm', ascending=False)
-    
-    if num > 0:
-        df = df.head(num)
-
-    logging.info(f"A total of {df.shape[0]} new stocks will be added to the database") 
-    
-    for _, row in tqdm(df.iterrows(), total = df.shape[0]):
-        
-        logging.info(f'Fetching company {row["companyname"]}')
-
-        exists = engine.execute(f"SELECT * FROM symbols WHERE companyname = \'{row['companyname']}\'")
-
-        # Check if we already have a symbol for the company 
-        if exists.rowcount == 0:
-            # Need to sleep for 13 seconds because api is limited to 5 API calls per minute
-            time.sleep(13)
-            # For some reason, the Alpha Vantage API symbol search endpoint works better when you don't give the full company name
-            # I've arbitrarily given it the first 15 characters
-            company_keywords = row['companyname'][:15]
-            response = requests.get(f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={company_keywords}&apikey={VANTAGE_API_KEY}')
-            symbol = None
-            # It checks for the first symbol of a company that is in the UK
-            for res in response.json()['bestMatches']:
-                if res["4. region"] == 'United Kingdom':
-                    symbol = res['1. symbol']
-                    break
-            # Write the symbol to the database. If not found, it will insert a 'None' value and we will have to manually fix that. 
-            statement = f"INSERT INTO symbols (companyname, symbol) VALUES (\'{row['companyname']}\',\'{symbol}\')"
-            engine.execute(statement)
-            logging.info(f"Inserted new symbol successfully for {row['companyname']}")
-            if symbol is None:
-                logging.info(f"Could not match stock {row['companyname']}")
-           
-        else:
-            logging.info(f"Stock ({row['companyname']}) already exists in db")
-
-def symbol_match_status():
-    """
-    (FUNCTION DEPRECATED - NOW DONE BY LSE SCRAPER SCRIPT)
-    Function to verify how many companies are left to be matched with a symbol
-    """
-    logging.info("Calculating symbol match status")
-    engine = get_pg_engine()
-    exists = engine.execute(f"SELECT * FROM symbols WHERE symbol = 'None'")
-    logging.info(f'There are {exists.rowcount} companies unmatched')
+class RateLimitExceeded(Exception):
+    def __init__(self,msg=None):
+        if msg is None:
+            msg = "API Rate Limit Exceeded"
 
 def fetch_price(symbol):
     """
@@ -121,6 +34,7 @@ def fetch_price(symbol):
     engine = get_pg_engine()
     # Check whether table for stock price already exists in the database, otherwise create it.
     engine.execute("CREATE TABLE IF NOT EXISTS price (timestamp date, open double precision,high double precision, low double precision, close double precision, volume double precision, symbol text)")
+    engine.execute("CREATE TABLE IF NOT EXISTS errors (symbol text, error text)")
     df_exist = pd.read_sql('SELECT * from price', engine)
 
     assert symbol not in df_exist['symbol'].tolist(), f'Price data already exists for this symbol {symbol}'
@@ -130,27 +44,21 @@ def fetch_price(symbol):
 
     df = pd.read_csv(f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={symbol}&apikey={VANTAGE_API_KEY}&datatype=csv')
     df['stock'] = symbol
-   
-    # Write the price data to the database
-    conn = engine.raw_connection()
-    cur = conn.cursor()
-    output = io.StringIO()
-    df.to_csv(output, sep='\t', header=False, index=False)
-    output.seek(0)
-    cur.copy_from(output, 'price', null="") # null values become ''
-    conn.commit()
-
-def manual_symbols():
-    """
-    (FUNCTION DEPRECATED - NOW DONE BY LSE SCRAPER SCRIPT)
-    Function which allows the user to manually input the missing symbols for the companies
-    """
-
-    engine = get_pg_engine()
-
-    df = pd.read_sql("SELECT * from symbols where symbol = 'None'", engine)
-
-    for _, row in df.iterrows():
-        symbol = input(f"What is the symbol for {row['companyname']}\n")
-
-        engine.execute(f"UPDATE symbols SET symbol = '{symbol}' WHERE companyname = '{row['companyname']}'")
+    
+    if 'Error' in df.iloc[0][0]:
+        logging.info(f'Could not fetch price for {symbol}, value will be stored in the errors table for manual verification')
+        engine.execute(f"INSERT INTO errors (symbol, error) VALUES ('{symbol}', 'Not matched to VantageAPI')")
+    elif 'Information' in df.iloc[0][0]:
+        logging.info(f"Could not fetch price for {symbol} as call limit was exceeded")
+        engine.execute(f"INSERT INTO errors (symbol, error) VALUES ('{symbol}', 'Rate limit exceeded')")
+        raise RateLimitExceeded
+    else:
+        # Write the price data to the database
+        logging.info(f"Symbol {symbol} found, inserting price data to database")
+        conn = engine.raw_connection()
+        cur = conn.cursor()
+        output = io.StringIO()
+        df.to_csv(output, sep='\t', header=False, index=False)
+        output.seek(0)
+        cur.copy_from(output, 'price', null="") # null values become ''
+        conn.commit()
